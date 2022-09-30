09bcff1f90bf9973332fbb1c018c21ee8c4054ed
Ruban Pavel <ruban.pavel@outlook.com>
2022-09-30 19:28:06 +0300

    123

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
 
+type propsType = {
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [todys,settodos] = useState<Array<propsType>>([])
+    console.log( "Hello")
+
+
+    const onClickHandler = () => {
+        settodos([])
+    }
+
+    useEffect(() => {
+        //onClickHandler()
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => settodos(json))
+    },[])
+
+
+    return (
+        <div className='App'>
+
+            <ul>
+                {todys.map(e => {
+                    return (
+                        <div>
+                            <li>{e.id}</li>
+                            <li>{e.title}</li>
+                            <li>{e.userId}</li>
+                        </div>
+                    )
+                })}
+            </ul>
+
+            <button onClick={onClickHandler}> Clean posts</button>
+        </div>
+    )
+        ;
 }
 
 export default App;
